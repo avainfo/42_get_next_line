352029688b989eac7483f6f95f7ad7e76a8162f4
Antonin Do Souto <avainfoconseils@gmail.com>
2025-11-11 12:54:18 +0000

    v1.0

--- get_next_line.c
+++ get_next_line.c
@@ -11,18 +11,69 @@
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include "get_next_line.h"
+
+static char	*read_file(int fd, char *stash)
+{
+	char	*buf;
+	ssize_t	bytes;
+
+	buf = malloc(BUFFER_SIZE + 1);
+	if (!buf)
+		return (NULL);
+	bytes = 1;
+	while (!ft_strchr(stash, '\n') && bytes > 0)
+	{
+		bytes = read(fd, buf, BUFFER_SIZE);
+		if (bytes < 0)
+			return (free(buf), NULL);
+		buf[bytes] = '\0';
+		stash = ft_strjoin(stash, buf);
+	}
+	free(buf);
+	return (stash);
+}
+
+static char	*extract_line(char *stash)
+{
+	size_t	i;
+	char	*line;
+
+	if (!stash[0])
+		return (NULL);
+	i = 0;
+	while (stash[i] && stash[i] != '\n')
+		i++;
+	line = ft_substr(stash, 0, stash[i] == '\n' ? i + 1 : i);
+	return (line);
+}
+
+static char	*clean_stash(char *stash)
+{
+	size_t	i;
+	char	*new_stash;
+
+	i = 0;
+	while (stash[i] && stash[i] != '\n')
+		i++;
+	if (!stash[i])
+		return (free(stash), NULL);
+	new_stash = ft_strdup(stash + i + 1);
+	free(stash);
+	return (new_stash);
+}
 
 char	*get_next_line(int fd)
 {
-	char	buff[BUFFER_SIZE];
-	int		r;
-	int		i;
+	static char	*stash;
+	char		*line;
 
-	r = read(fd, buff, BUFFER_SIZE);
-	if (r < 0)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	i = 0;
-	while (i < r)
-		i++;
-	return (buff);
+	stash = read_file(fd, stash);
+	if (!stash)
+		return (NULL);
+	line = extract_line(stash);
+	stash = clean_stash(stash);
+	return (line);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -13,6 +13,18 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+
 char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(char *s, int c);
+size_t	ft_strlen(char *s);
+char	*ft_substr(char *s, unsigned int start, size_t len);
+char	*ft_strdup(char *s);
 
 #endif
